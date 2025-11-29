6a7124de41ff1efab2179745c7abb3e9c77ec388
brentvc <bchionh@gmail.com>
2025-11-30 10:22:21 +1100

    Initial Commit

---
+++ MonashDashboard/MonashDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonashDashboardApp.swift
+//  MonashDashboard
+//
+//  Created by Brent Chionh on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MonashDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonashDashboardTests/MonashDashboardTests.swift
@@ -0,0 +1,16 @@
+//
+//  MonashDashboardTests.swift
+//  MonashDashboardTests
+//
+//  Created by Brent Chionh on 30/11/2025.
+//
+
+import Testing
+
+struct MonashDashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
